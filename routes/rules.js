5d00dbed01b3e578572af4e5124c04824e7bdbe9
tomki1 <tomki@oregonstate.edu>
2020-10-17 16:12:29 -0700

    update

--- routes/rules.js
+++ routes/rules.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/rules, (req, res, next) => {
-    res.render('rules', { title: "Rules" });
+router.get('/rules', (req, res, next) => {
+    res.render('rules', { title: "Collector's Trading Platform" });
 });
 
 module.exports = router;
