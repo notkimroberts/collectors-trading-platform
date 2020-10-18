3a1125dcbaa82cd29d028fbea53bde1f550cae70
tomki1 <tomki@oregonstate.edu>
2020-10-17 18:30:42 -0700

    update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dotenv = require('dotenv')
+const hbs = require('hbs')
 
 dotenv.config()
 
@@ -28,6 +29,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/collectible', collectibleRouter); // TODO: Sample route, to be deleted.
 app.use('/rules', rulesRouter);
+
+hbs.registerPartials(path.join(__dirname, '/views/partials'))
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
