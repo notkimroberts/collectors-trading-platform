7c229a94d0e7564253d5ada41ea11dee3f704a2d
tomki1 <tomki@oregonstate.edu>
2020-10-17 18:49:56 -0700

    update

--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('index', { title: "Collector's Trading Platform" });
+    res.render('profile', { title: "Profile" });
 });
 
 module.exports = router;
--- routes/rules.js
+++ routes/rules.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('index', { title: "Collector's Trading Platform" });
+    res.render('rules', { title: "Rules" });
 });
 
 module.exports = router;
