f7f2b3df9902219d64f68126ca3329fb5c7ed4b3
tomki1 <tomki@oregonstate.edu>
2020-10-18 18:09:39 -0700

    removed header from .hbs files and placed it in layout.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/', (req, res, next) => {
     res.render('index', { title: "Collector's Trading Platform" });
 });
