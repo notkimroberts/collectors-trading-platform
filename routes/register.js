2c80afee942df710865145e11b6e6f48bd96230e
Jason Lim <lim.jasonn@gmail.com>
2020-10-23 16:51:00 -0700

    pg instead of mysql notation

--- routes/register.js
+++ routes/register.js
@@ -9,11 +9,11 @@ module.exports = router;
 
 router.post('/', function(req, res){
     console.log(req.body)
-    var mysql = req.app.get('mysql');
+    var pg = req.app.get('pg');
     var sql = "INSERT INTO Collector(is_admin, username, password, fullname, address, city, state, country, zipcode,contact_email, phone_number) VALUES (?,?,?,?)";
     var inserts = [req.body.is_admin, req.body.username, req.body.password, req.body.fullname, req.body.address,
         req.body.city, req.body.state, req.body.country, req.body.contact_email, req.body.phone_number]; 
-    sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+    sql = pg.pool.query(sql,inserts,function(error, results, fields){
         if(error){
             console.log(JSON.stringify(error))
             res.write(JSON.stringify(error));
