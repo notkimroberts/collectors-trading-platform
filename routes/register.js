da4916c9418d3f71457c5060fa94b43751a0bc78
Jason Lim <lim.jasonn@gmail.com>
2020-10-24 00:04:50 -0700

    try1

--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('knex')
 
 router.get('/', (req, res) => {
     res.render('register', { title: "Register" });
@@ -8,7 +9,7 @@ router.get('/', (req, res) => {
 
 router.post('/', function(req, res){
     console.log(req.body)
-    knex(collector).insert(JSON.parse(req.body.is_admin, req.body.username, req.body.password, req.body.fullname, req.body.address,
+    knex('collector').insert(JSON.parse(req.body.is_admin, req.body.username, req.body.password, req.body.fullname, req.body.address,
         req.body.city, req.body.state, req.body.country, req.body.contact_email, req.body.phone_number));
         res.render('register', { title: "Register" });
     });
--- sql/knexfile.js
+++ sql/knexfile.js
@@ -1,5 +1,4 @@
 const dotenv = require('dotenv')
-const knex = require('knex')
 
 dotenv.config({ path: '../.env' })
 
