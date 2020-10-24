eda94bd59c46d699255afe4514ae5e293b64f208
Jason Lim <lim.jasonn@gmail.com>
2020-10-23 22:38:15 -0700

    removing double postgres signin

--- sql/knexfile.js
+++ sql/knexfile.js
@@ -1,17 +1,17 @@
-const dotenv = require('dotenv')
+// const dotenv = require('dotenv')
 
-dotenv.config({ path: '../.env' })
+// dotenv.config({ path: '../.env' })
 
-module.exports = {
-    client: 'postgresql',
-    connection: {
-        host : process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
-        user : process.env.DATABASE_USER,
-        password : process.env.DATABASE_PASSWORD,
-        database : process.env.DATABASE_NAME,
-        ssl: { rejectUnauthorized: false }
-    },
-    migrations: { tableName: 'knex_migrations' },
-    seeds: { directory: './seeds' },
-}
+// module.exports = {
+//     client: 'postgresql',
+//     connection: {
+//         host : process.env.DATABASE_HOST,
+//         port: process.env.DATABASE_PORT,
+//         user : process.env.DATABASE_USER,
+//         password : process.env.DATABASE_PASSWORD,
+//         database : process.env.DATABASE_NAME,
+//         ssl: { rejectUnauthorized: false }
+//     },
+//     migrations: { tableName: 'knex_migrations' },
+//     seeds: { directory: './seeds' },
+// }
