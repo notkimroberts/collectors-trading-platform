ae72ddb99acc2558ccea4a981c28f5fe108c2849
Jason Lim <lim.jasonn@gmail.com>
2020-10-23 23:48:12 -0700

    register.js knex

--- routes/register.js
+++ routes/register.js
@@ -5,6 +5,10 @@ router.get('/', (req, res) => {
     res.render('register', { title: "Register" });
 });
 
+knex(collector).insert(JSON.parse(req.body.is_admin, req.body.username, req.body.password, req.body.fullname, req.body.address,
+    req.body.city, req.body.state, req.body.country, req.body.contact_email, req.body.phone_number))
+
+
 // router.post('/', function(req, res){
 //     console.log(req.body)
 //     var pg = req.app.get('pg');
