0eb545296e1b107a0cd174651cf153ca8969041a
Jason Lim <lim.jasonn@gmail.com>
2020-10-23 17:00:07 -0700

    syntax err

--- routes/register.js
+++ routes/register.js
@@ -10,7 +10,7 @@ module.exports = router;
 router.post('/', function(req, res){
     console.log(req.body)
     var pg = req.app.get('pg');
-    var sql = "INSERT INTO Collector(is_admin, username, password, fullname, address, city, state, country, zipcode,contact_email, phone_number) VALUES (?,?,?,?)";
+    var sql = "INSERT INTO collector(is_admin, username, password, fullname, address, city, state, country, zipcode,contact_email, phone_number) VALUES (?,?,?,?)";
     var inserts = [req.body.is_admin, req.body.username, req.body.password, req.body.fullname, req.body.address,
         req.body.city, req.body.state, req.body.country, req.body.contact_email, req.body.phone_number]; 
     sql = pg.pool.query(sql,inserts,function(error, results, fields){
