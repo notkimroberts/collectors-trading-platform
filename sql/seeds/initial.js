0c8dd4ec6131a2e9956b7da26782c018f2fd94d8
Jason Lim <lim.jasonn@gmail.com>
2020-10-24 00:08:57 -0700

    not sure what syntax for initial.js knex is

--- sql/seeds/initial.js
+++ sql/seeds/initial.js
@@ -1,13 +1,13 @@
 exports.seed = (knex) => {
-  const collectible = knex('collectible')
-    .del()
-    .then(() => {
-        return knex('collectible').insert([
-            { collectible_id: 1, name: 'test', image_url: 'google.com', attributes: { color: 'green', style: 'cool' }, total_quantity: 445 },
-            { collectible_id: 2, name: 'test', image_url: 'google.com', attributes: { color: 'green', style: 'cool' }, total_quantity: 445 },
-            { collectible_id: 3, name: 'test', image_url: 'google.com', attributes: { color: 'green', style: 'cool' }, total_quantity: 445 },
-        ]);
-    });
+//   const collectible = knex('collectible')
+//     .del()
+//     .then(() => {
+//         return knex('collectible').insert([
+//             { collectible_id: 1, name: 'test', image_url: 'google.com', attributes: { color: 'green', style: 'cool' }, total_quantity: 445 },
+//             { collectible_id: 2, name: 'test', image_url: 'google.com', attributes: { color: 'green', style: 'cool' }, total_quantity: 445 },
+//             { collectible_id: 3, name: 'test', image_url: 'google.com', attributes: { color: 'green', style: 'cool' }, total_quantity: 445 },
+//         ]);
+//     });
 
     const collector = knex('collector')
     .del()
@@ -26,7 +26,7 @@ exports.seed = (knex) => {
     });
 
     return Promise.all([
-        collectible,
         collector,
+        // collectible,
     ])
 }
