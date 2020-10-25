a0f15a258f39cb533054d3aca54761bcb597dc28
Jason Lim <email@example.com>
2020-10-25 15:09:07 -0700

    collector.js integration w/ users.js

--- models/collector.js
+++ models/collector.js
@@ -9,5 +9,22 @@ module.exports = {
             ])
         })
         return Promise.all([collector])
-    }
+      },
+    getOne: function (collector_id) {
+        return knex('collector').where('collector_id', collector_id).first();
+      },
+    
+      getOneByEmail: function (email) {
+        return knex('collector').where('email', email).first();
+      },
+      
+      getOneByUsername: function (username) {
+        return knex('collector').where('username', username).first();
+      },
+      
+      create: function(collector) {
+        return knex('collector').insert(collector, 'collector_id').then(ids => {
+          return ids[0];
+        });
+      }
 }
