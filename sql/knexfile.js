9b52d14ae219d7ba2622e54c0fea5cd42b709684
Kim Roberts <kimroberts@Kims-MacBook-Pro-16.local>
2020-10-30 16:27:43 -0700

    fix: use production db

--- sql/knexfile.js
+++ sql/knexfile.js
@@ -5,16 +5,14 @@ dotenv.config({ path: '../.env' })
 
 module.exports = {
     client: 'postgresql',
-    connection: 'postgres://localhost/ctp',
-    // TODO: Uncomment this for deployment.
-    // connection: {
-    //     host : process.env.DATABASE_HOST,
-    //     port: process.env.DATABASE_PORT,
-    //     user : process.env.DATABASE_USER,
-    //     password : process.env.DATABASE_PASSWORD,
-    //     database : process.env.DATABASE_NAME,
-    //     ssl: { rejectUnauthorized: false }
-    // },
+    connection: {
+        host : process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        user : process.env.DATABASE_USER,
+        password : process.env.DATABASE_PASSWORD,
+        database : process.env.DATABASE_NAME,
+        ssl: { rejectUnauthorized: false }
+    },
     migrations: { tableName: 'knex_migrations' },
     seeds: { directory: './seeds' },
 }
