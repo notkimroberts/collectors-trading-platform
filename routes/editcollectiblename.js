c2331134b5410d23d1d5a8a34214b2ee17f8e4d0
tomki1 <tomki@oregonstate.edu>
2020-10-30 22:18:35 -0700

    input validation for edit collectible

--- routes/editcollectiblename.js
+++ routes/editcollectiblename.js
@@ -1,4 +1,5 @@
 const Collectible = require('../models/Collectible.js');
+const Collector = require('../models/Collector.js');
 const express = require('express');
 const knex = require('../connection')
 const router = express.Router();
@@ -12,6 +13,7 @@ router.get('/', (req, res, next) => {
 function validCollectible(collectible) {
     
     const validName = typeof collectible.name == 'string' && collectible.name.trim() != '';
+
     return validName;
     
 }
@@ -25,12 +27,42 @@ router.post('/', async (req, res, next) => {
     // if input is valid
     if(validCollectible(req.body)) {
         
+        const { name, collectible_id } = req.body;
 
-
-                        await knex('collectible').where({collectible_id: req.body.collectible_id}).update({ name: req.body.name});
-                        res.sendStatus(200);                    
-                                       
+        // Check if existing collectible name
+        if (await Collectible.getByName(name)) {
+            res.render('editcollectiblename', { 
+                    message: 'That collectible name already exists in the database. unique names only',
+                    messageClass: 'alert-danger'
                 }
+            )
+            return
+        }
+
+
+
+        // Check if existing collector_id
+        if (!(await Collectible.getById(collectible_id))) {
+            res.render('editcollectiblename', { 
+                    message: 'That collectible_id does not exist',
+                    messageClass: 'alert-danger'
+                }
+            )
+            return
+        }
+
+
+
+
+
+
+
+        await knex('collectible').where({collectible_id: collectible_id}).update({ name: name});
+        
+                
+        res.redirect('/');
+                    
+    }   
 
 
     // else fields were not valid
