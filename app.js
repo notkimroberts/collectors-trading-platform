4610b9685d828c67d528b66f8311cbe9e0330cf9
tomki1 <tomki@oregonstate.edu>
2020-10-30 23:47:46 -0700

    edited name for editcollectible

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ const quizRouter = require('./routes/quiz');
 const quizResultRouter = require('./routes/quizResult');
 const registerRouter = require('./routes/register');
 const rulesRouter = require('./routes/rules');
-const editcollectiblenameRouter = require('./routes/editcollectiblename');
+const editcollectibleRouter = require('./routes/editcollectible');
 
 app.use('/', indexRouter);
 app.use('/add-collectible', addCollectibleRouter);
@@ -66,7 +66,7 @@ app.use('/quiz', quizRouter);
 app.use('/quiz-result', quizResultRouter);
 app.use('/register', registerRouter);
 app.use('/rules', rulesRouter);
-app.use('/editcollectiblename', editcollectiblenameRouter);
+app.use('/editcollectible', editcollectibleRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
