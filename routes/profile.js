7c42c6878949185af0f42f940d61f0ecaf7abfaa
tomki1 <tomki@oregonstate.edu>
2020-11-03 03:21:53 -0800

    render user's has_quantity collecitbles to profile page

--- routes/profile.js
+++ routes/profile.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { requireAuth } = require('../utils')
 
 
-router.get('/', requireAuth, async (req, res, next) => {
+/* router.get('/', requireAuth, async (req, res, next) => {
     const { email, phone_number, username, collector_id} = req.user;
     res.render('profile', { 
         email: email,
@@ -12,22 +12,28 @@ router.get('/', requireAuth, async (req, res, next) => {
         title: `Collector's Trading Platform | ${username}`,
         username: username,
         collector_id: collector_id
-    });
-});
+    }); 
+}); */
 
 
-
-    router.get('/:id', requireAuth, async (req, res, next) => {
-        const { id } = req.params;
+// render user's collectibles if has_quantity is greater than 0
+    router.get('/', requireAuth, async (req, res, next) => {
+        // const { id } = req.params;
         const { email, phone_number, username, collector_id} = req.user;
         const collections = await knex('collection')
-        .select('wants_quantity', 'has_quantity', 'willing_to_trade_quantity')
-        .where({ collector_id: collector_id })
-        .andWhere( {collectible_id: id });
+        .select(['collection.collectible_id', 'collection.has_quantity', 'collection.wants_quantity', 'collection.willing_to_trade_quantity', 'collectible.name'])
+        .join('collectible', 'collectible.collectible_id', 'collection.collectible_id')
+        .where('collector_id', collector_id )
+        .andWhere('collection.has_quantity', '>', 0);
     
         console.log(collections);
     
         res.render('profile', { 
+            email: email,
+            phone_number: phone_number,
+            title: `Collector's Trading Platform | ${username}`,
+            username: username,
+            collector_id: collector_id,
             collection: collections,
         });
     });
