c4c986b807d629b0316b055eaf6673db29fa9e13
Kim Roberts <robekimb@oregonstate.edu>
2020-11-06 16:33:26 -0800

    fix: logout

--- routes/logout.js
+++ routes/logout.js
@@ -2,20 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', requireAuth, (req, res, next) => {
-    res.render('logout', { title: "Logout" });
-});
+router.get('/', (req, res, next) => {
+    res.render('logout', { title: "Logout" });}
+);
 
-router.post('/', requireAuth, function (req, res) {
-    res.clearCookie(authTokens);
-    res.redirect('/login');
-    // req.session.destroy(err => {
-    //     if (err) {
-    //         return res.redirect('/');
-    //     }
-    //     res.clearCookie(authTokens);
-    //     res.redirect('/login');
-    // })
-});
 
 module.exports = router;
