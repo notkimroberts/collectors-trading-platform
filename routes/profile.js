b6cd85994c8328b1dc1c5395808b75c82d24bb28
Jason Lim <email@example.com>
2020-11-07 21:52:22 -0800

    has, wants, for trade in profile

--- routes/profile.js
+++ routes/profile.js
@@ -33,24 +33,24 @@ const { ensureLoggedIn } = require('../auth/middleware')
         // user's has collectibles if has_quantity is greater than 0
         const collectionsHas = await knex('collection')
         .select(['collection.collectible_id', 'collection.has_quantity', 'collection.wants_quantity', 'collection.willing_to_trade_quantity', 'collectible.name'])
-        .join('collectible', 'collectible.collectible_id', 'collection.collectible_id')
-        .where('collector_id', req.signedCookies.user_id )
-        .andWhere('collection.has_quantity', '>', 0);
+        .join('collectible', 'collectible.collectible_id', 'collection.collectible_id');
+        // .where('collector_id', req.signedCookies.user_id )
+        // .andWhere('collection.has_quantity', '>', 0);
 
         // user's wants collectibles if has_quantity is greater than 0
         const collectionsWants = await knex('collection')
         .select(['collection.collectible_id', 'collection.has_quantity', 'collection.wants_quantity', 'collection.willing_to_trade_quantity', 'collectible.name'])
-        .join('collectible', 'collectible.collectible_id', 'collection.collectible_id')
-        .where('collector_id', req.signedCookies.user_id )
-        .andWhere('collection.wants_quantity', '>', 0);
+        .join('collectible', 'collectible.collectible_id', 'collection.collectible_id');
+        // .where('collector_id', req.signedCookies.user_id )
+        // .andWhere('collection.wants_quantity', '>', 0);
     
 
         // user's wants collectibles if has_quantity is greater than 0
         const collectionsWillingToTrade = await knex('collection')
         .select(['collection.collectible_id', 'collection.has_quantity', 'collection.wants_quantity', 'collection.willing_to_trade_quantity', 'collectible.name'])
-        .join('collectible', 'collectible.collectible_id', 'collection.collectible_id')
-        .where('collector_id', req.signedCookies.user_id )
-        .andWhere('collection.willing_to_trade_quantity', '>', 0);
+        .join('collectible', 'collectible.collectible_id', 'collection.collectible_id');
+        // .where('collector_id', req.signedCookies.user_id )
+        // .andWhere('collection.willing_to_trade_quantity', '>', 0);
 
 
         console.log(collectorData);
