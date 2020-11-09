74c6e7e1117d135508adcecc957245605f6283d5
Jason Lim <email@example.com>
2020-11-08 20:52:36 -0800

    test

--- routes/profile.js
+++ routes/profile.js
@@ -18,8 +18,6 @@ const { ensureLoggedIn } = require('../auth/middleware')
     }); 
 }); */
 
-
-
     router.get('/', ensureLoggedIn, async (req, res, next) => {
         // const { id } = req.params;
         //console.log(user_id);
@@ -33,24 +31,24 @@ const { ensureLoggedIn } = require('../auth/middleware')
         // user's has collectibles if has_quantity is greater than 0
         const collectionsHas = await knex('collection')
         .select(['collection.collectible_id', 'collection.has_quantity', 'collection.wants_quantity', 'collection.willing_to_trade_quantity', 'collectible.name'])
-        .join('collectible', 'collectible.collectible_id', 'collection.collectible_id');
-        // .where('collector_id', req.signedCookies.user_id )
-        // .andWhere('collection.has_quantity', '>', 0);
+        .join('collectible', 'collectible.collectible_id', 'collection.collectible_id')
+        .where('collector_id', req.signedCookies.user_id )
+        .andWhere('collection.has_quantity', '>', 0);
 
         // user's wants collectibles if has_quantity is greater than 0
         const collectionsWants = await knex('collection')
         .select(['collection.collectible_id', 'collection.has_quantity', 'collection.wants_quantity', 'collection.willing_to_trade_quantity', 'collectible.name'])
-        .join('collectible', 'collectible.collectible_id', 'collection.collectible_id');
-        // .where('collector_id', req.signedCookies.user_id )
-        // .andWhere('collection.wants_quantity', '>', 0);
+        .join('collectible', 'collectible.collectible_id', 'collection.collectible_id')
+        .where('collector_id', req.signedCookies.user_id )
+        .andWhere('collection.wants_quantity', '>', 0);
     
 
         // user's wants collectibles if has_quantity is greater than 0
         const collectionsWillingToTrade = await knex('collection')
         .select(['collection.collectible_id', 'collection.has_quantity', 'collection.wants_quantity', 'collection.willing_to_trade_quantity', 'collectible.name'])
-        .join('collectible', 'collectible.collectible_id', 'collection.collectible_id');
-        // .where('collector_id', req.signedCookies.user_id )
-        // .andWhere('collection.willing_to_trade_quantity', '>', 0);
+        .join('collectible', 'collectible.collectible_id', 'collection.collectible_id')
+        .where('collector_id', req.signedCookies.user_id )
+        .andWhere('collection.willing_to_trade_quantity', '>', 0);
 
 
         console.log(collectorData);
@@ -68,6 +66,8 @@ const { ensureLoggedIn } = require('../auth/middleware')
         });
     });
 
+
+    router.post('/submit')
     
 
 module.exports = router;
