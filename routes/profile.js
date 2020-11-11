a4799f054bed8a19987b6d426e1fa995ed205654
Jason Lim <email@example.com>
2020-11-10 23:37:43 -0800

    .

--- routes/profile.js
+++ routes/profile.js
@@ -61,11 +61,7 @@ router.get('/', ensureLoggedIn, async (req, res, next) => {
     router.post('/', async (req, res, next) => 
     { 
         const userId = req.signedCookies.user_id      
-        const collectorData = await knex('collector')
-        .select('username', 'email', 'phone_number', 'collector_id')
-        .where('collector_id', userId );
-
-
+     
         const q1 = req.body.has_quantity;
         const q2 = req.body.wants_quantity;
         const q3 = req.body.willing_to_trade_quantity;
@@ -77,15 +73,17 @@ router.get('/', ensureLoggedIn, async (req, res, next) => {
         console.log(userId);
         console.log(collectible_id1);
 
-        
+        const collectorData = await knex('collector')
+        .select('username', 'email', 'phone_number', 'collector_id')
+        .where('collector_id', userId );
+
         await knex('collection')
             .where({collector_id: userId})
             .andWhere({collectible_id: collectible_id1})
             .update({has_quantity: q1})
             .update({wants_quantity: q2})
             .update({willing_to_trade_quantity: q3 });
-
-        res.redirect(`profile`);
+        res.redirect(`/profile`);
     });
 
 module.exports = router;
