798009660cff410701d9a24fd3f2ee63ea686a6b
Jason Lim <email@example.com>
2020-11-10 23:37:10 -0800

    zz

--- routes/collectible.js
+++ routes/collectible.js
@@ -130,35 +130,14 @@ router.post('/:id', async (req, res, next) =>
     const q1 = req.body.has_quantity;
     const q2 = req.body.wants_quantity;
     const q3 = req.body.willing_to_trade_quantity;
-<<<<<<< HEAD
-    const collectorSelected = req.body.collector_id;
-    const collectible_id1 = req.body.collectible_id;
-=======
     const collectible_id1 = req.body.collectible_id;
 
 
->>>>>>> 3b9d6a0b9ca2a5aa62283f54150b2bec62731229
     console.log(q1);
     console.log(q2);
     console.log(q3);
     console.log(userId);
     console.log(collectible_id1);
-<<<<<<< HEAD
-    await knex('collection')
-        .where({collector_id: req.signedCookies.user_id})
-        .andWhere({collectible_id: collectible_id1})
-        .update({has_quantity: q1})
-        .update({wants_quantity: q2})
-        .update({willing_to_trade_quantity: q3 });
-
-    res.render('profile', { 
-        collector: collectorData,
-        collector_id: req.signedCookies.user_id,
-        collectionHas: collectionsHas,
-        collectionWants: collectionsWants,
-        collectionWillingToTrade: collectionsWillingToTrade
-    });
-=======
 
     const collectibles = await knex('collectible')
         .join('collectible_type', 'collectible.collectible_type_id', '=', 'collectible_type.collectible_type_id')
@@ -174,7 +153,6 @@ router.post('/:id', async (req, res, next) =>
 
     res.redirect(`/collectible/${collectible_id1}`);
 
->>>>>>> 3b9d6a0b9ca2a5aa62283f54150b2bec62731229
 });
 
 router.get('/image/:id', async (req, res, next) => { 
