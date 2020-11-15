8af25c1663b43ac5d502f5ae6a080523376a59e6
Kim Roberts <robekimb@oregonstate.edu>
2020-11-14 18:17:28 -0800

    feat: show users with collectible

--- routes/collectible.js
+++ routes/collectible.js
@@ -113,9 +113,16 @@ router.get('/:id', async (req, res, next) => {
 
         // see if there's already a collector_id and collectible_id pair in the table
         const collectionExists = await knex('collection')
-        .select(['collectible_id'])
-        .where('collector_id', userId)
-        .where('collectible_id', id);
+            .select(['collectible_id'])
+            .where('collector_id', userId)
+            .where('collectible_id', id);
+        
+        // users with collectible
+        const usersWithCollectible = await knex('collection')
+            .select(['collector.collector_id', 'collector.username'])
+            .join('collector', 'collector.collector_id', 'collection.collector_id')
+            .where('collectible_id', '=', id)
+            .andWhere('willing_to_trade_quantity', '>', 0)
 
         // if results, render users collectibles to the form
         if (collectionExists.length > 0) {
@@ -129,6 +136,7 @@ router.get('/:id', async (req, res, next) => {
                 collectionWants: collectionsWants,
                 collectionWillingToTrade: collectionsWillingToTrade,
                 somethingInCollection,
+                usersWithCollectible,
                 id
             });
             return;
@@ -142,6 +150,7 @@ router.get('/:id', async (req, res, next) => {
                 collector_id: userId,
                 collectible: collectibles,
                 nothingInCollection,
+                usersWithCollectible,
                 id
             });
             return;
