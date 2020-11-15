a6e8ab8cb2800189ea056551619b3984099d2cce
Kim Roberts <robekimb@oregonstate.edu>
2020-11-15 12:04:01 -0800

    fix: bug in showing trade matches

--- routes/collector.js
+++ routes/collector.js
@@ -48,22 +48,22 @@ router.get('/:id', async (req, res, next) => {
 });
 
 router.get('/trade/:id', async (req, res, next) => { 
-    const { id } = req.params; // other collector's id
-    const userId = req.signedCookies.user_id; // logged in collector's id
+    const currentUserId = req.signedCookies.user_id
+    const otherUserId = req.params.id
 
     // logged in user data
     const currentUser = await knex('collector')
         .select('collector_id', 'username', 'email', 'phone_number', 'is_admin')
-        .where('collector_id', userId).first()
+        .where('collector_id', currentUserId).first()
 
     // the other collector's user data
     const otherUser = await knex('collector')
         .select('collector_id', 'username', 'email', 'phone_number', 'is_admin')
-        .where({ collector_id: id }).first()
+        .where({ collector_id: otherUserId }).first()
 
     const currentUserWants = await knex('collection')
         .select(['collectible_id'])
-        .where('collector_id', '=', userId)
+        .where('collector_id', '=', currentUserId)
         .andWhere('wants_quantity', '>', 0)
 
     const otherUserWants = await knex('collection')
@@ -79,14 +79,14 @@ router.get('/trade/:id', async (req, res, next) => {
     const currentUserToOtherUser = await knex('collection')
         .select(['collection.collector_id', 'collection.collectible_id', 'collectible.name', 'collection.willing_to_trade_quantity'])
         .join('collectible', 'collection.collectible_id', 'collectible.collectible_id')
-        .where('collector_id','!=', currentUser.collector_id)
+        .where('collector_id','=', currentUserId)
         .whereIn('collection.collectible_id', otherUserWantsCollectibleIds)
         .andWhere('willing_to_trade_quantity', '>', 0)
     
     const otherUserToCurrentUser = await knex('collection')
         .select(['collection.collector_id', 'collection.collectible_id', 'collectible.name', 'collection.willing_to_trade_quantity'])
         .join('collectible', 'collection.collectible_id', 'collectible.collectible_id')
-        .where('collector_id','=', otherUser.collector_id)
+        .where('collector_id','=', otherUserId)
         .whereIn('collection.collectible_id', currentUserWantsCollectibleIds)
         .andWhere('willing_to_trade_quantity', '>', 0)
 
