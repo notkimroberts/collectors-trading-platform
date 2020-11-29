ceac8115b9ca11e38022e4be137cbec05ad3be77
tomki1 <tomki@oregonstate.edu>
2020-11-28 20:47:12 -0800

    comment

--- public/js/checkimagesize.js
+++ public/js/checkimagesize.js
@@ -1,5 +1,5 @@
 // https://stackoverflow.com/questions/1601455/how-to-check-file-input-size-with-jquery
-// make sure image is less than 20KB
+// make sure image is less than 40KB
 $("input[type='file']").on("change", function () {
     // if file size is greater than 40KB
     if(this.files[0].size > 40000) {
