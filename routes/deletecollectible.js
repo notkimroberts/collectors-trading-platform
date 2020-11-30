7bb0b9a7fde7295851e5b5715a5031700bc044b4
tomki1 <tomki@oregonstate.edu>
2020-11-30 00:42:35 -0800

    clean up delete collectible

--- routes/deletecollectible.js
+++ routes/deletecollectible.js
@@ -9,17 +9,11 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', async (req, res, next) => {
-
     const { collectible_id } = req.body;
-    const collectibleData = await knex('collectible')    
-    .select('collectible_type_id')
-    .where({ collectible_id: collectible_id }).first();
-    const typeSelected = collectibleData.collectible_type_id;
     const userId = req.signedCookies.user_id;
 
     // Check if existing collectible_id
     if (!(await Collectible.getById(collectible_id))) {
-        console.log("Hello");
         res.render('deletecollectible', { 
                 message: 'That collectible id does not exist',
                 messageClass: 'alert-danger'
@@ -27,212 +21,40 @@ router.post('/', async (req, res, next) => {
         )
         return
     }
-    if (typeSelected == '1') {
-        const collectibleData = await knex('collectible')
-        .select('collectible_id', 'collectible_type_id')
-        .where({collectible_id: collectible_id});
-        knex.table('collectible').pluck('collectible_type_id').where('collectible_id', collectible_id).then(async function(ids) { 
-            const collectibleType = '1';    
-            var s = ids.includes(collectibleType);
-            if (s == true)   {
-                const collectorData = await knex('collector')
-                .select('username', 'email', 'phone_number', 'collector_id')
-                .where('collector_id', userId );
-                knex.table('collector').pluck('is_admin').where('collector_id', userId ).then(async function(ids) { 
-                    const collectibleType = '1';  
-                    var n = ids.includes(collectibleType);
-                    const collectibleAll = '6';
-                    var z = ids.includes(collectibleAll);
-                    if (n == true || z == true)
-                    {
-                        await knex('collectible')
-                        .where({collectible_id: collectible_id})
-                        .delete();
-                    
-                            res.render('deletecollectible', { 
-                                message: 'Collectible has been deleted',
-                                messageClass: 'alert-success'
-                            }
-                        )  
-                        return
-                }
-                else{
-                    res.render('deletecollectible', { 
-                        message: 'You do not have the admin privilege to edit this collectible',
-                        messageClass: 'alert-danger'
-                        }   
-                    )
-                        return
-                }
-    });
-}
-        });
+
+    const collectibleData = await knex('collectible')    
+    .select('collectible_type_id')
+    .where({ collectible_id: collectible_id }).first();
+    
+    const collectorData = await knex('collector')
+    .select('is_admin')
+    .where('collector_id', userId ).first();
+
+    const collectibleType = collectibleData.collectible_type_id;
+    const adminType = collectorData.is_admin;
+
+    // if the admin type does not match the collectible type and the admin type is not an all admin, render error
+    if (collectibleType != adminType && adminType != 6) {
+        res.render('deletecollectible', { 
+            message: 'You do not have the admin privilege to edit this collectible',
+            messageClass: 'alert-danger'
+            }   
+        )
+            return
     }
-    if (typeSelected == '2') {
-        const collectibleData = await knex('collectible')
-        .select('collectible_id', 'collectible_type_id')
-        .where({collectible_id: collectible_id});
-        knex.table('collectible').pluck('collectible_type_id').where('collectible_id', collectible_id).then(async function(ids) { 
-            const collectibleType = '2';    
-            var s = ids.includes(collectibleType);
-            if (s == true)   {
-                const collectorData = await knex('collector')
-                .select('username', 'email', 'phone_number', 'collector_id')
-                .where('collector_id', userId );
-                knex.table('collector').pluck('is_admin').where('collector_id', userId ).then(async function(ids) { 
-                    const collectibleType = '2';  
-                    var n = ids.includes(collectibleType);
-                    const collectibleAll = '6';
-                    var z = ids.includes(collectibleAll);
-                    if (n == true || z == true)
-                    {
-                        await knex('collectible')
-                        .where({collectible_id: collectible_id})
-                        .delete();
-                    
-                            res.render('deletecollectible', { 
-                                message: 'Collectible has been deleted',
-                                messageClass: 'alert-success'
-                            }
-                        )  
-                        return
-                    }
-                    else{
-                        res.render('deletecollectible', { 
-                            message: 'You do not have the admin privilege to edit this collectible',
-                            messageClass: 'alert-danger'
-                            }   
-                        )
-                            return
-                    }
-                });
-            }
-                    });
-                }
-                if (typeSelected == '3') {
-                    const collectibleData = await knex('collectible')
-        .select('collectible_id', 'collectible_type_id')
-        .where({collectible_id: collectible_id});
-        knex.table('collectible').pluck('collectible_type_id').where('collectible_id', collectible_id).then(async function(ids) { 
-            const collectibleType = '3';    
-            var s = ids.includes(collectibleType);
-            if (s == true)   {
-                const collectorData = await knex('collector')
-                .select('username', 'email', 'phone_number', 'collector_id')
-                .where('collector_id', userId );
-                knex.table('collector').pluck('is_admin').where('collector_id', userId ).then(async function(ids) { 
-                    const collectibleType = '3';  
-                    var n = ids.includes(collectibleType);
-                    const collectibleAll = '6';
-                    var z = ids.includes(collectibleAll);
-                    if (n == true || z == true)
-                    {
-                        await knex('collectible')
-                        .where({collectible_id: collectible_id})
-                        .delete();
-                    
-                            res.render('deletecollectible', { 
-                                message: 'Collectible has been deleted',
-                                messageClass: 'alert-success'
-                            }
-                        )  
-                        return
-                    }
-                    else{
-                        res.render('deletecollectible', { 
-                            message: 'You do not have the admin privilege to edit this collectible',
-                            messageClass: 'alert-danger'
-                            }   
-                        )
-                            return
-                    }
-                });
-            }
-                    });
-                }
-                if (typeSelected == '4') {
-                    console.log('hello');
-        const collectibleData = await knex('collectible')
-        .select('collectible_id', 'collectible_type_id')
-        .where({collectible_id: collectible_id});
-        knex.table('collectible').pluck('collectible_type_id').where('collectible_id', collectible_id).then(async function(ids) { 
-            const collectibleType = '4';    
-            var s = ids.includes(collectibleType);
-            if (s == true)   {
-                const collectorData = await knex('collector')
-                .select('username', 'email', 'phone_number', 'collector_id')
-                .where('collector_id', userId );
-                knex.table('collector').pluck('is_admin').where('collector_id', userId ).then(async function(ids) { 
-                    const collectibleType = '4';  
-                    var n = ids.includes(collectibleType);
-                    const collectibleAll = '6';
-                    var z = ids.includes(collectibleAll);
-                    if (n == true || z == true)
-                    {
-                        await knex('collectible')
-                        .where({collectible_id: collectible_id})
-                        .delete();
-                    
-                            res.render('deletecollectible', { 
-                                message: 'Collectible has been deleted',
-                                messageClass: 'alert-success'
-                            }
-                        )  
-                        return
-                    }
-                    else{
-                        res.render('deletecollectible', { 
-                            message: 'You do not have the admin privilege to edit this collectible',
-                            messageClass: 'alert-danger'
-                            }   
-                        )
-                            return
-                    }
-                });
-            }
-                    });
-                }
-                if (typeSelected == '5') {
-                    const collectibleData = await knex('collectible')
-        .select('collectible_id', 'collectible_type_id')
-        .where({collectible_id: collectible_id});
-        knex.table('collectible').pluck('collectible_type_id').where('collectible_id', collectible_id).then(async function(ids) { 
-            const collectibleType = '5';    
-            var s = ids.includes(collectibleType);
-            if (s == true)   {
-                const collectorData = await knex('collector')
-                .select('username', 'email', 'phone_number', 'collector_id')
-                .where('collector_id', userId );
-                knex.table('collector').pluck('is_admin').where('collector_id', userId ).then(async function(ids) { 
-                    const collectibleType = '5';  
-                    var n = ids.includes(collectibleType);
-                    const collectibleAll = '6';
-                    var z = ids.includes(collectibleAll);
-                    if (n == true || z == true)
-                    {
-                        await knex('collectible')
-                        .where({collectible_id: collectible_id})
-                        .delete();
-                    
-                            res.render('deletecollectible', { 
-                                message: 'Collectible has been deleted',
-                                messageClass: 'alert-success'
-                            }
-                        )  
-                        return
-                    }
-                    else{
-                        res.render('deletecollectible', { 
-                            message: 'You do not have the admin privilege to edit this collectible',
-                            messageClass: 'alert-danger'
-                            }   
-                        )
-                            return
-                    }
-                });
-            }
-                    });
-                }
-            });
+
+    // delete collectible
+    await knex('collectible')
+    .where({collectible_id: collectible_id})
+    .delete();
+
+        res.render('deletecollectible', { 
+            message: 'Collectible has been deleted',
+            messageClass: 'alert-success'
+        }
+    )  
+    return
+});
+
 
 module.exports = router;
