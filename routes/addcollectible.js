ee61d33f97d262a2177644554849682a4f013fce
tomki1 <tomki@oregonstate.edu>
2020-12-03 14:15:34 -0800

    repopulate fields after add collectible error

--- routes/addcollectible.js
+++ routes/addcollectible.js
@@ -53,20 +53,98 @@ router.post('/', async (req, res, next) => {
     const collectorData = await knex('collector')    
     .select('is_admin')
     .where({ collector_id: userId }).first();
+
+    //auto populates type selection on add collectible screen
+    if (collectorData.is_admin == 1) {
+        var selectLego = 1;
+    }
+
+    if (collectorData.is_admin == 2) {
+        var selectFunko = 1;
+    }
+    if (collectorData.is_admin == 3) {
+        var selectPusheen = 1;
+    }
+
+    if (collectorData.is_admin == 4) {
+        var selectPokemon = 1;
+    }
+    else if  (collectorData.is_admin == 5) {
+        var selectHotWheels = 1;
+    }
     
     const userAdminType = collectorData.is_admin;
+
+    theName = req.body.name;
+    thePieceCount = req.body.piece_count;
+    theSetNumber = req.body.set_number;
+    theTheme = req.body.theme;
+    theDesigner = req.body.designed_by;
+    theNumber = req.body.number;
+    theLine = req.body.line;
+    theProductType1 = req.body.product_type1;
+    theSeason = req.body.season;
+    theProductType = req.body.product_type;
+    theGeneration = req.body.generation;
+    theNumber1 = req.body.number1;
+    theSeries = req.body.series;
+    theYearReleased1 = req.body.year_released1;
 
     if(validCollectible(req.body)) {
         if(userAdminType !=typeSelected && userAdminType != "6") { //if user admin is not the collectible type or if not all admin
             res.render('addcollectible', { 
-                message: 'You do not have the admin privilege to edit this collectible',
+                selectLego,
+                selectFunko,
+                selectPusheen,
+                selectPokemon,
+                selectHotWheels,
+                theName,
+                message: 'You do not have the admin privilege to add this collectible type.',
                 messageClass: 'alert-danger'
                 }   
             )
             return
         }
         if (await Collectible.getByName(req.body.name)) { //name already exists
+
+            // The admin type is already correct if in this statement. rendering the typeSelected will be correct for every admin type
+            if (typeSelected == 1) {
+                var selectLego = 1;
+            }
+        
+            if (typeSelected == 2) {
+                var selectFunko = 1;
+            }
+            if (typeSelected == 3) {
+                var selectPusheen = 1;
+            }
+        
+            if (typeSelected == 4) {
+                var selectPokemon = 1;
+            }
+            else if  (typeSelected == 5) {
+                var selectHotWheels = 1;
+            }
+
             res.render('addcollectible', { 
+                    selectLego,
+                    selectFunko,
+                    selectPusheen,
+                    selectPokemon,
+                    selectHotWheels,
+                    thePieceCount,
+                    theSetNumber,
+                    theTheme,
+                    theDesigner,
+                    theNumber,
+                    theLine,
+                    theProductType1,
+                    theSeason,
+                    theProductType,
+                    theGeneration,
+                    theNumber1,
+                    theSeries,
+                    theYearReleased1,
                     message: 'That collectible name already exists in the database. Unique names only.',
                     messageClass: 'alert-danger'
                 }
@@ -75,6 +153,25 @@ router.post('/', async (req, res, next) => {
         }
         if (!req.files) {
             res.render('addcollectible', {  //need to upload image
+                    selectLego,
+                    selectFunko,
+                    selectPusheen,
+                    selectPokemon,
+                    selectHotWheels,
+                    theName,
+                    thePieceCount,
+                    theSetNumber,
+                    theTheme,
+                    theDesigner,
+                    theNumber,
+                    theLine,
+                    theProductType1,
+                    theSeason,
+                    theProductType,
+                    theGeneration,
+                    theNumber1,
+                    theSeries,
+                    theYearReleased1,
                     message: 'Please choose a jpeg image to upload',
                     messageClass: 'alert-danger'
                 }
@@ -86,8 +183,15 @@ router.post('/', async (req, res, next) => {
         .then(async (collectible) => {
             if (!collectible) {
                 if (typeSelected == "1") { //if lego input below lego attributes
+                    var selectLego = 1;
                     if (!req.body.piece_count) {
                         res.render('addcollectible', { 
+                                selectLego,
+                                theName,
+                                thePieceCount,
+                                theSetNumber,
+                                theTheme,
+                                theDesigner,
                                 message: 'Please add piece count',
                                 messageClass: 'alert-danger'
                             }
@@ -96,6 +200,12 @@ router.post('/', async (req, res, next) => {
                     }
                     if (!req.body.set_number) {
                         res.render('addcollectible', { 
+                                selectLego,
+                                theName,
+                                thePieceCount,
+                                theSetNumber,
+                                theTheme,
+                                theDesigner,
                                 message: 'Please add set_number',
                                 messageClass: 'alert-danger'
                             }
@@ -104,6 +214,12 @@ router.post('/', async (req, res, next) => {
                     }
                     if (!req.body.theme) {
                         res.render('addcollectible', { 
+                                selectLego,
+                                theName,
+                                thePieceCount,
+                                theSetNumber,
+                                theTheme,
+                                theDesigner,
                                 message: 'Please add theme',
                                 messageClass: 'alert-danger'
                             }
@@ -112,6 +228,12 @@ router.post('/', async (req, res, next) => {
                     }
                     if (!req.body.designed_by) {
                         res.render('addcollectible', { 
+                                selectLego,
+                                theName,
+                                thePieceCount,
+                                theSetNumber,
+                                theTheme,
+                                theDesigner,
                                 message: 'Please add designer',
                                 messageClass: 'alert-danger'
                             }
@@ -135,8 +257,13 @@ router.post('/', async (req, res, next) => {
                     return;
                 }
                 else if (typeSelected == "2") { 
+                    var selectFunko = 1;
                     if (!req.body.number) {
                         res.render('addcollectible', { 
+                                selectFunko,
+                                theName,
+                                theNumber,
+                                theLine,
                                 message: 'Please add number',
                                 messageClass: 'alert-danger'
                             }
@@ -145,6 +272,10 @@ router.post('/', async (req, res, next) => {
                     }
                     if (!req.body.line) {
                         res.render('addcollectible', { 
+                                selectFunko,
+                                theName,
+                                theNumber,
+                                theLine,
                                 message: 'Please add line',
                                 messageClass: 'alert-danger'
                             }
@@ -168,8 +299,13 @@ router.post('/', async (req, res, next) => {
                     return;
                 }      
                 else if (typeSelected == "3") {
+                    var selectPusheen = 1;
                     if (!req.body.product_type1) {
                         res.render('addcollectible', { 
+                                selectPusheen,
+                                theName,
+                                theProductType1,
+                                theSeason,
                                 message: 'Please add product type',
                                 messageClass: 'alert-danger'
                             }
@@ -178,6 +314,10 @@ router.post('/', async (req, res, next) => {
                     }
                     if (!req.body.season) {
                         res.render('addcollectible', { 
+                                selectPusheen,
+                                theName,
+                                theProductType1,
+                                theSeason,
                                 message: 'Please add season/holiday',
                                 messageClass: 'alert-danger'
                             }
@@ -201,8 +341,13 @@ router.post('/', async (req, res, next) => {
                     return;
                 }
                 else if (typeSelected == "4") {
+                    var selectPokemon = 1;
                     if (!req.body.product_type) {
                         res.render('addcollectible', { 
+                                selectPokemon,
+                                theName,
+                                theProductType,
+                                theGeneration,
                                 message: 'Please add product type',
                                 messageClass: 'alert-danger'
                             }
@@ -212,6 +357,10 @@ router.post('/', async (req, res, next) => {
 
                     if (!req.body.generation) {
                         res.render('addcollectible', { 
+                                selectPokemon,
+                                theName,
+                                theProductType,
+                                theGeneration,
                                 message: 'Please add generation',
                                 messageClass: 'alert-danger'
                             }
@@ -235,8 +384,14 @@ router.post('/', async (req, res, next) => {
                     return;
                 }
                 else if (typeSelected == "5") {
+                    var selectHotWheels = 1;
                     if (!req.body.number1) {
                         res.render('addcollectible', { 
+                                selectHotWheels,
+                                theName,
+                                theNumber1,
+                                theSeries,
+                                theYearReleased1,
                                 message: 'Please add number',
                                 messageClass: 'alert-danger'
                             }
@@ -245,6 +400,11 @@ router.post('/', async (req, res, next) => {
                     }
                     if (!req.body.series) {
                         res.render('addcollectible', { 
+                                selectHotWheels,
+                                theName,
+                                theNumber1,
+                                theSeries,
+                                theYearReleased1,
                                 message: 'Please add series',
                                 messageClass: 'alert-danger'
                             }
@@ -253,6 +413,11 @@ router.post('/', async (req, res, next) => {
                     }
                     if (!req.body.year_released1) {
                         res.render('addcollectible', { 
+                                selectHotWheels,
+                                theName,
+                                theNumber1,
+                                theSeries,
+                                theYearReleased1,
                                 message: 'Please add year released',
                                 messageClass: 'alert-danger'
                             }
@@ -278,6 +443,11 @@ router.post('/', async (req, res, next) => {
                 }
                 else {
                     res.render('addcollectible', { 
+                            selectLego,
+                            selectFunko,
+                            selectPusheen,
+                            selectPokemon,
+                            selectHotWheels,
                         message: 'That collectible type does not exist',
                         messageClass: 'alert-danger'
                         }   
@@ -294,6 +464,11 @@ router.post('/', async (req, res, next) => {
     // else name is not valid
     else {
         res.render('addcollectible', { 
+            selectLego,
+            selectFunko,
+            selectPusheen,
+            selectPokemon,
+            selectHotWheels,
             message: 'Invalid name',
             messageClass: 'alert-danger'
             }   
