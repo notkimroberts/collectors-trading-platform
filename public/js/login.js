cad6603c90d189f1d0d7ca7c8e362146bbd2ec00
Jason Lim <email@example.com>
2020-12-12 15:37:27 -0800

    added login

---
+++ public/js/login.js
@@ -0,0 +1,23 @@
+/* const { error } = require("jquery"); */
+// https://www.youtube.com/watch?v=YFxsqHD6JUc&
+$(() => {
+
+    $('form').submit((event) => {
+        event.preventDefault();
+        const collector = getUserFromForm();
+
+
+        login(collector)
+            .then(result => {
+               setIdRedirect(result);
+            }).catch(err => {
+                console.error(err);
+                showErrorMessage(err.responseJSON.message);                
+            });
+    });
+});
+
+
+function login(collector) {
+    return $.post(`${AUTH_URL}/login`, collector); 
+}
